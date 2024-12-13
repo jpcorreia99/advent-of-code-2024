3722295381109a8c36b309905437ec72db0d2831
jpcorreia99 <jpcorreia99@gmail.com>
2024-12-13 22:11:01 +0000

    Day 7 part 2

--- 07/07.py
+++ 07/07.py
@@ -5,7 +5,7 @@ with open(source, "r") as file:
     all_lines = file.readlines()
 
 # Part 1
-#for each line, explore the solution space until a match is found or all
+# for each line, explore the solution space until a match is found or all
 # possibilities are exhausted
 nums_lines = []
 for line in all_lines:
@@ -18,15 +18,55 @@ def verify_operation(total, operation, nums, cur_total):
     if not nums:
         return total == cur_total
 
-    cur_total = cur_total + nums[0] if operation == '+' else cur_total * nums[0]
+    cur_total = cur_total + nums[0] if operation == "+" else cur_total * nums[0]
     nums = nums[1:]
-    
-    return (verify_operation(total, '+', nums, cur_total) or verify_operation(total, '*', nums, cur_total))
+
+    return verify_operation(total, "+", nums, cur_total) or verify_operation(
+        total, "*", nums, cur_total
+    )
 
 
 res = 0
-for total, nums  in nums_lines:
-    if verify_operation(total, '+', nums, 0) or verify_operation(total, '*', nums, 1):
+for total, nums in nums_lines:
+    if verify_operation(total, "+", nums, 0) or verify_operation(total, "*", nums, 1):
         res += total
 
-print(res) 
+# 5540634308362
+print(res)
+
+
+# Part 2
+# Same thing with a new branch in the space exploration
+def new_verify_operation(total, operation, nums, cur_total):
+    if not nums or cur_total > total:
+        return total == cur_total
+
+    match operation:
+        case "+":
+            cur_total = cur_total + nums[0]
+        case "*":
+            cur_total = cur_total * nums[0]
+        case "||":
+            cur_total = int(str(cur_total) + str(nums[0]))
+
+    nums = nums[1:]
+
+    return (
+        new_verify_operation(total, "+", nums, cur_total)
+        or new_verify_operation(total, "*", nums, cur_total)
+        or new_verify_operation(total, "||", nums, cur_total)
+    )
+
+
+res = 0
+for total, nums in nums_lines:
+    start = nums.pop(0)
+    if (
+        new_verify_operation(total, "+", nums, start)
+        or new_verify_operation(total, "*", nums, start)
+        or new_verify_operation(total, "||", nums, start)
+    ):
+        res += total
+
+# 472290821152397
+print(res)
